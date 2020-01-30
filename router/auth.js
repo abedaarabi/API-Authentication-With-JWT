a282f7062d5e4e912d1e022eac6c3090eb9b27fb
Abdulrahman Mohamad Aarabi <abma@moe.dk>
2020-01-30 12:46:35 +0100

    Validate data before make user!

--- router/auth.js
+++ router/auth.js
@@ -1,32 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../model/User");
-const Joi = require("joi");
-
-//VALIDATION
-
-const schema = {
-  name: Joi.string()
-    .min(6)
-    .required(),
-  email: Joi.string()
-    .min(6)
-    .required()
-    .email({ minDomainAtoms: 2 }),
-  password: Joi.string()
-    .min(6)
-    .required()
-};
+const { registerValidation } = require("../validation");
 
 //router
 router.post("/register", async (req, res) => {
   //LET'S VALIDATE THE DATA BEFORE WE MAKE USER
-  const { error } = Joi.validate(req.body, schema);
+  const { error } = registerValidation(req.body);
+  // const error = registerValidation(req.body).error;
 
-  // res.send(error.details[0].message);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //CHECK IN USER ALRADY IN DATABASE
+  const userExist = await User.findOne({ email: req.body.email });
+
+  if (userExist) {
+    return res.status(400).send("USER ALRADY EXIST!");
   }
+
+  //CREATE NEW USER!
   user = new User({
     name: req.body.name,
     email: req.body.email,
--- validation.js
+++ validation.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 //VALIDATION
 
-const userValidation = () => {
+const registerValidation = data => {
   const schema = {
     name: Joi.string()
       .min(6)
@@ -9,7 +9,20 @@ const userValidation = () => {
     email: Joi.string()
       .min(6)
       .required()
-      .email({ minDomainAtoms: 2 }),
+      .email(),
+    password: Joi.string()
+      .min(6)
+      .required()
+  };
+  return Joi.validate(data, schema);
+};
+
+const logInValidation = data => {
+  const schema = {
+    email: Joi.string()
+      .min(6)
+      .required()
+      .email(),
     password: Joi.string()
       .min(6)
       .required()
